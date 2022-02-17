2f2bc316c88577ea4cd2bf12af1546c4b2a0ad95
Siva Sai Prakash <ssprayudu121@gmail.com>
2022-02-17 17:23:34 +0530

    added basic view to project

---
+++ script.js
@@ -0,0 +1,15 @@
+// btn = document.querySelector('button');
+
+// container = document.querySelector('body');
+
+// function enableDarkMode() {
+//     if (container.style.backgroundColor === 'black') {
+//         container.style.backgroundColor = '#fff';
+//         document.querySelector('h1').style.color = '#000';
+
+//     } else {
+//         container.style.backgroundColor = 'black';
+//         document.querySelector('h1').style.color = '#fff';
+//     }
+// }
+// btn.addEventListener('click', enableDarkMode);
